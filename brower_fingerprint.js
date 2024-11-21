d4070c02388e9cc46c8d60e532897d47f0c07f3e
deepakyadav987 <deepakyadavyadav987@gmail.com>
2024-11-21 12:35:56 +0530

    first commit

---
+++ brower_fingerprint.js
@@ -0,0 +1,25 @@
+async function getDeviceFingerprint() {
+    const deviceData = {
+        platform : navigator.platform,
+        navigator:navigator.hardwareConcurrency,
+        cpuCores: navigator.hardwareConcurrency || 'Unknown', // Number of logical CPU cores (device-specific)
+        
+    };
+    const deviceDataString = JSON.stringify(deviceData);
+
+    // Hashing the combined data to generate a fingerprint
+    const hashedFingerprint = await generateSha256Hash(deviceDataString);
+
+    console.log("Device Data:", deviceData);
+    console.log("Hashed Device Fingerprint:", hashedFingerprint);
+}
+
+async function generateSha256Hash(data) {
+    const encoder = new TextEncoder();
+    const dataBuffer = encoder.encode(data);
+    const hashBuffer = await crypto.subtle.digest('SHA-256', dataBuffer);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    return hashArray.map(byte => byte.toString(16).padStart(2, '0')).join('');
+}
+
+getDeviceFingerprint();
